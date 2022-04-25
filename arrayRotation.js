987153cea928738940a2c7e762c0f3e5b7d70e7a
stepha22cy <104310070+stepha22cy@users.noreply.github.com>
2022-04-25 13:42:22 +0300

    Add files via upload

---
+++ arrayRotation.js
@@ -0,0 +1,15 @@
+function arrayRotation(array,rotationNum){
+
+    for(let i=0;i<rotationNum;i++){
+        let currentNum=array.shift()
+        array.push(currentNum);
+    }
+    
+        console.log(array.join(' '));
+    
+    
+}
+arrayRotation([51,47,32,61,21],2)
+
+// arrayRotation[32, 21, 61, 1], 4	
+// arrayRotation[2, 4, 15, 31], 5
